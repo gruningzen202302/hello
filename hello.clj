218d36d227f51d4e282a3d4d8b04f724ebd37840
Vir_Th_Nix_Os <gruningzen202302@gmail.com>
2023-05-05 03:01:39 +0100

    feat:added alias in deps to connect to a repl

--- hello.clj
+++ hello.clj
@@ -1,6 +1,9 @@
 (ns hello 
-  (:require [clj-time.core :as t]
-            [clj.time.format :as f]))
-(* 700 450)
+;;  (:require [clj-time.core :as t]
+;;            [clj.time.format :as f])
+  )
+
+(println  (* 700 450))
+
 (defn -main []
   (println "hello from C l o j u r e !!!"))
