48ba3032fd1b439bf9f45f7804adaa8dafc964aa
AlbertoGruning-MacOS <kimono76.live@gmail.com>
2023-05-21 10:40:31 -0300

    feat:move html files to folder

---
+++ book/clj/ch002book copy 7.clj
@@ -0,0 +1,8 @@
+(ns welcome-to-clojure
+  (:require [clojure.repl :refer [source apropos dir pst doc find-doc]]
+            [clojure.string :as string]
+            [clojure.test :refer [is are]]))
+
+(comment
+
+  text)
