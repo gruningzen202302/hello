b30cc9f122731d38f157595ee0a023616ce051bf
AlbertoGruning-MacOS <kimono76.live@gmail.com>
2023-05-26 20:25:55 -0300

    fix:remove unused refs to solve warnings

--- ch003book.clj
+++ ch003book.clj
@@ -1,7 +1,4 @@
-(ns ch003book
-  (:require [clojure.repl :refer [source apropos dir pst doc find-doc]]
-            [clojure.string :as string]
-            [clojure.test :refer [is are]]))
+(ns ch003book)
 
 (comment
 
