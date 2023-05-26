0a632ae5117f9498d78783901408ac1368e171bd
AlbertoGruning-MacOS <kimono76.live@gmail.com>
2023-05-26 09:33:36 -0300

    fix:typo

--- ch001book.clj
+++ ch001book.clj
@@ -643,7 +643,11 @@
   
   (vec #{:a :b :c})
   (:Vec #{:a :b :c})
-  (:clojure.core/Vec #{:a :b :c})
+
+
+
+
+
 ;;       [:a :c :b]
 ;;       
 ;;   
@@ -978,8 +982,7 @@
 ;;   var \_gaq = \_gaq || \[\]; \_gaq.push(\['\_setAccount', 'UA-9527251-1'\]); \_gaq.push(\['\_trackPageview'\]); (function() { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.locationotocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')\[0\]; s.parentNode.insertBefore(ga, s); })();
 ;;   
 ;;  
-  
-  )
+     )
 
 (comment
   ;; = re-matches - Example 1 = 
