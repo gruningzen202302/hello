72a5b10ffc6acaa0fdc8431e60f7a6779f42bfb7
AlbertoGruning-MacOS <kimono76.live@gmail.com>
2023-06-19 16:57:11 -0300

    feat: create bits.clj

---
+++ bits.clj
@@ -0,0 +1,20 @@
+(ns bits)
+
+(comment
+  (defn to-left [n]
+    (bit-shift-left 1 n))
+
+  (to-left 4)
+
+  (bit-xor
+   2r1100
+   2r1110)
+;;2
+
+  (Integer/toBinaryString
+   (bit-xor
+    2r1100
+    2r1110))
+;;10
+
+  )
