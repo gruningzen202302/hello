751808b996c8c1f8dc8740c6680349c0ca53145c
AlbertoGruning-MacOS <kimono76.live@gmail.com>
2023-06-21 21:02:53 -0300

    feat: handle a string to convert it into a matrix

--- matrix.clj
+++ matrix.clj
@@ -2,11 +2,14 @@
   (:require [clojure.string :as str]))
 
 (def raw 
-  "0 4 1 9
-   3 0 6 11
-   4 1 0 2
-   6 5 -4 0")
+  "0 4 1 9 3 0 6 11 4 1 0 2 6 5 -4 0")
 
 (def lines (str/split-lines raw))
 
 (print lines)
+
+(def str-to-num (map #(str/split % #" ") lines))
+
+str-to-num
+
+(map identity str-to-num)
